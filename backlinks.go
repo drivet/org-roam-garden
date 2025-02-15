e093a8924e322918e0d54642fac4d3c52a51cfb6
Desmond Rivet <desmond.rivet@gmail.com>
2025-02-15 17:12:48 -0500

    Updates backlink program

---
+++ backlinks.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"fmt"
+	_ "modernc.org/sqlite"
+	"gopkg.in/yaml.v3"
+)
+
+const OrgRoamDb = "./org-roam/.org-roam.db"
+const OrgRoamRoot = "/home/dcr/org-roam-garden/org-roam"
+const BacklinkOutputDir = "/home/dcr/org-roam-garden/data"
+
+type Backlink struct {
+    File string
+    Title string
+}
+
+func getBacklinks(db *sql.DB, file string) []Backlink {
+	row, err := db.Query("SELECT file, title FROM nodes WHERE id IN (SELECT DISTINCT l.source AS source FROM nodes n, links l WHERE n.file = ? AND n.id = l.dest)", file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	var backlinks []Backlink
+	for row.Next() {
+		b := Backlink{}
+		err := row.Scan(&b.File, &b.Title)
+		if err != nil {
+			log.Fatal(err)
+		}
+		backlinks = append(backlinks, b)
+	}
+	return backlinks
+}
+
+func main() {
+	db, err := sql.Open("sqlite", OrgRoamDb)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filerow, err := db.Query("SELECT DISTINCT file from nodes")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer filerow.Close()
+	file2bl := make(map[string][]Backlink)
+	for filerow.Next() {
+		var file string
+		err := filerow.Scan(&file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		file2bl[file] = getBacklinks(db, file)
+	}
+	
+	blout, err := yaml.Marshal(file2bl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.MkdirAll(BacklinkOutputDir, 0777)
+	f, err := os.OpenFile(fmt.Sprintf("%s/%s", BacklinkOutputDir, "backlinks.yaml"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	f.WriteString(string(blout))
+}
