7dd43ff6a001b1dacea2f3cdae81fb50267b1712
Desmond Rivet <desmond.rivet@gmail.com>
2025-03-03 09:09:33 -0500

    Add lastmod to every note, add tags to notes

--- lastmodified.sh
+++ lastmodified.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-for i in org-roam/**.org; do
+for i in org-roam/*.org; do
     lm=`git log -1 --format=%ci $i`
-    echo "$i: $lm"
+    sed -r -i "s/(^\#\+date: .*$)/\1\n#+hugo_lastmod: $lm/" $i
 done
-#git log -1 --format=%ci org-roam/20220408123339-emacs_mark.org
+for i in org-roam/**/*.org; do
+    lm=`git log -1 --format=%ci $i`
+    sed -r -i "s/(^\#\+date: .*$)/\1\n#+hugo_lastmod: $lm/" $i
+done
